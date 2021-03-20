967fe0172d69b67493e8a03d6a66d781057a8b9c
GauthierLory <gauthier.lory@protonmail.com>
2021-03-20 22:22:53 +0100

    add npm scripts, eslint, Prettier, gitignore

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 const Pet = ({ name, animal, breed }) => {
-    return React.createElement("div", {},[
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
 };
 
-
 const App = () => {
-    return React.createElement(
-        "div", // quel type de tag on call
-        { id: "something-important" }, // attributs qu'on passe au component ou child
-        [
-            React.createElement("h1", {}, "Adopt me"), // children
-            React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havenese"}),
-            React.createElement(Pet, { name: "Pepper", animal: "Cat", breed: "Mixed"}),
-            React.createElement(Pet, { name: "Banana", animal: "Bird", breed: "Cockatiel"}),
-        ]
-    );
+  return React.createElement(
+    "div", // quel type de tag on call
+    { id: "something-important" }, // attributs qu'on passe au component ou child
+    [
+      React.createElement("h1", {}, "Adopt me"), // children
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havenese",
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Cat",
+        breed: "Mixed",
+      }),
+      React.createElement(Pet, {
+        name: "Banana",
+        animal: "Bird",
+        breed: "Cockatiel",
+      }),
+    ]
+  );
 };
 
 ReactDOM.render(
-    React.createElement(App), // render le component App
-    document.getElementById("root")
+  React.createElement(App), // render le component App
+  document.getElementById("root")
 );
