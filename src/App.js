11b9756edf69f4a9340726e4db9dc92316b03b6d
GauthierLory <gauthier.lory@protonmail.com>
2021-03-20 20:20:11 +0100

    createElement with args

---
+++ src/App.js
@@ -0,0 +1,26 @@
+const Pet = (props) => {
+    return React.createElement("div", {},[
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ]);
+};
+
+
+const App = () => {
+    return React.createElement(
+        "div", // quel type de tag on call
+        { id: "something-important" }, // attributs qu'on passe au component ou child
+        [
+            React.createElement("h1", {}, "Adopt me"), // children
+            React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havenese"}),
+            React.createElement(Pet, { name: "Pepper", animal: "Cat", breed: "Mixed"}),
+            React.createElement(Pet, { name: "Banana", animal: "Bird", breed: "Cockatiel"}),
+        ]
+    );
+};
+
+ReactDOM.render(
+    React.createElement(App), // render le component App
+    document.getElementById("root")
+);
