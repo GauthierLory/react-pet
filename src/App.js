8b7cc37a02a8d2dcff6e480c9acd4d027be84948
GauthierLory <gauthier.lory@protonmail.com>
2021-03-22 13:21:56 +0100

    useState, custome hooks, update ESLint, bring in petfinder client

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement(
-    "div", // quel type de tag on call
-    { id: "something-important" }, // attributs qu'on passe au component ou child
-    [
-      React.createElement("h1", {}, "Adopt me"), // children
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havenese",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Cat",
-        breed: "Mixed",
-      }),
-      React.createElement(Pet, {
-        name: "Banana",
-        animal: "Bird",
-        breed: "Cockatiel",
-      }),
-    ]
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me !</h1>
+      <SearchParams />
+    </div>
   );
 };
 
-render(
-  React.createElement(App), // render le component App
-  document.getElementById("root")
-);
+render(<App />, document.getElementById("root"));
